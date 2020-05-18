613390f2b5d34f46164d77f3f753296c77aebde8
Divyansh Jain <jaindivyansh2010@gmail.com>
2020-05-18 20:07:08 +0530

    assignment3

---
+++ assignment3/src/com/sapients/fig_to_word/fig_to_wordRunner.java
@@ -0,0 +1,11 @@
+package com.sapients.fig_to_word;
+
+public class fig_to_wordRunner {
+	
+	public static void main(String[] args) {
+		long  number =  123456789123456789l;
+		
+		System.out.println(figToWord.fig_to_word(number));
+		
+	}
+}
